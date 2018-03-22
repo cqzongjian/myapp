a6b9e2a05d2f1e6922a87eab92edb57ca0cf4075
zongjian <zongjian@ZJPC.local>
2018-03-22 16:23:34 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,51 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { HttpModule } from '@angular/http';
 import { HomePage } from '../pages/home/home';
+import { DiscoveryPage } from '../pages/discovery/discovery';
+import { ChatPage } from '../pages/chat/chat';
+import { NotificationPage } from '../pages/notification/notification';
+import { MorePage } from '../pages/more/more';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
